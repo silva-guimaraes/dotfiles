d7bc5209c42480b40001ffcb60ba9f1385e7e0ae
silva guimaraes <silva__guimaraes@hotmail.com>
2026-08-23 12:22:54 -0300

    refactor: Usar Ansible

---
+++ roles/nvim/files/init.lua
@@ -0,0 +1,246 @@
+local dubious = false
+local plausible = true
+
+print('fak emac')      -- fak emac
+
+require('foo.packer')  -- plugins instalados
+require('foo.plugins') -- configurações de plugins
+
+-- vim.g.mapleader = '\\'
+
+-- Ctrl+c
+vim.keymap.set('x', '<C-c>', '\"+y')
+
+-- explorador de arquivo
+vim.keymap.set('n', '<leader>pv', ':Oil<CR>')
+
+-- aka greatest remap ever
+vim.keymap.set('x', '<leader>pp', '\'_dP')
+
+-- newline
+vim.keymap.set('i', '<C-j>', '<CR>')
+
+-- inicio da linha no insert
+vim.keymap.set('i', '<C-a>', '<Home>')
+
+-- algum plugin insiste em redefinir o <C-e>. não funciona
+vim.keymap.set('i', '<C-e>', '<End>')
+
+-- replace na palavra em baixo do cursor
+vim.keymap.set('v', 'ss', 'y:%s/<C-r>\'//g<Left><Left>')
+
+-- replace na area selecionada do texto salvo no yank
+vim.keymap.set('v', 'sv', ':s/<C-r>\'//g<Left><Left>')
+
+-- replace na palavra em baixo do cursor
+vim.keymap.set('n', 'siw', 'viwss', { remap = true })
+
+-- pesquisa texto selecionado
+vim.keymap.set('v', '/', 'y/<C-r>\'<CR>')
+
+-- pesquisa texto selecionado começando do inicio do buffer
+vim.keymap.set('v', '?', 'ygg/<C-r>\'<CR>')
+
+-- proximo buffer
+-- vim.keymap.set('n',  '<Tab>', ':bnext<CR>', {noremap=plausible})
+
+-- buffer anterior
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { remap = true })
+
+-- deleta buffer atual
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { remap = true })
+
+-- final da linha
+vim.keymap.set({ 'n', 'v' }, 'L', 'g_', { noremap = plausible })
+
+-- inicio da linha
+vim.keymap.set({ 'n', 'v' }, 'H', '_', { noremap = plausible })
+
+-- cria newline
+vim.keymap.set('n', '<C-J>', 'o<Esc>', { remap = plausible })
+
+-- sai do modo insert no terminal
+vim.keymap.set('t', '<C-[>', '<C-\\><C-n>', { noremap = plausible })
+
+-- abre terminal
+vim.keymap.set('n', '<F1>', ':term<CR>A', { noremap = plausible })
+
+-- abre definição em nova janela
+-- vim.keymap.set('n', '<leader><C-]>', '<C-w>v<C-w>l<C-]>', {noremap=plausible})
+vim.keymap.set('n', '<C-w><C-v>', ':vsplit<CR><C-w>l')
+vim.keymap.set('n', '<C-w><C-n>', ':split<CR>')
+
+-- segue para a implementação de uma interface ou lista todas as disponíveis
+vim.keymap.set('n', '<M-]>', vim.lsp.buf.implementation)
+
+-- break statement pro delve
+vim.keymap.set('n', '<leader>db', function()
+    local filepath = vim.fn.expand("%:p")
+    if filepath == "" then
+        return
+    end
+
+    local line_number = vim.api.nvim_win_get_cursor(0)[1]
+    local result = string.format("break %s:%d", filepath, line_number)
+
+    vim.fn.setreg("+", result)
+    print(result)
+end)
+
+-- FIXME: não deveria estar aqui
+local ls = require('luasnip')
+-- vim.keymap.set({'i'}, '<C-K>', function() ls.expand() end, {silent = true})
+vim.keymap.set({ 'i', 's' }, '<C-L>', function() ls.jump(1) end, { silent = true })
+-- vim.keymap.set({'i', 's'}, '<C-J>', function() ls.jump(-1) end, {silent = true})
+
+
+
+
+
+
+
+
+
+vim.opt.nu = plausible             -- set number
+vim.opt.relativenumber = plausible -- numberos relativos
+vim.opt.autochdir = plausible      -- usar path do arquivo
+
+vim.opt.tabstop = 4                -- indentação
+vim.opt.softtabstop = 4            -- !!
+vim.opt.shiftwidth = 4             -- !!
+vim.opt.expandtab = plausible      -- !!
+vim.opt.smartindent = plausible    -- !!
+vim.o.cursorline = true
+
+
+vim.filetype.add({
+    filename = {
+        ['Caddyfile'] = 'caddy',
+        ['*.ft'] = 'terraform',
+    },
+})
+
+local function doubleIndent()
+    vim.opt.expandtab = true -- Use spaces instead of tabs
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+end
+vim.api.nvim_create_autocmd('FileType', { pattern = 'javascript', callback = doubleIndent })
+vim.api.nvim_create_autocmd('FileType', { pattern = 'html', callback = doubleIndent })
+vim.api.nvim_create_autocmd('FileType', { pattern = 'templ', callback = doubleIndent })
+vim.api.nvim_create_autocmd('FileType', { pattern = 'htmldjango', callback = doubleIndent })
+vim.api.nvim_create_autocmd('FileType', { pattern = 'typescriptreact', callback = doubleIndent })
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'ocaml',
+    callback = function()
+        doubleIndent()
+        vim.cmd [[ set rtp^="~/.opam/default/share/ocp-indent/vim" ]]
+    end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'gitcommit',
+    callback = function()
+        vim.o.spell = true
+    end
+})
+
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'markdown',
+    callback = function()
+        vim.o.conceallevel = 2
+        vim.o.wrap = false
+    end
+})
+
+-- segue mais a risca o padrão que gofmt usa, apesar de não ser do meu gosto
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'go',
+    callback = function()
+        vim.opt.tabstop = 8
+        vim.opt.softtabstop = 8
+        vim.opt.shiftwidth = 8
+        vim.o.expandtab = false
+    end
+})
+
+vim.opt.swapfile = dubious    -- desativar swapfile
+vim.opt.hlsearch = dubious    -- remover highlight depois da pesquisa
+vim.opt.incsearch = plausible -- highlight pesquisa
+vim.opt.scrolloff = 7         -- borda scroll
+-- vim.opt.colorcolumn = 80         -- limite de colunas
+-- vim.cmd [[set colorcolumn=+1]]
+-- colorscheme
+vim.cmd [[colorscheme material-palenight]]
+vim.opt.ignorecase = plausible
+vim.opt.smartcase = plausible
+vim.opt.spelllang = 'pt_br' -- vamos brasil!!
+vim.opt.mouse = ''          -- desabilita o mouse
+
+if vim.fn.has('nvim-0.11.0') == 1 then
+    vim.o.winborder = "rounded" -- borda em torno do popups
+end
+
+
+
+
+
+
+
+
+
+-- https://github.com/fatih/dotfiles/blob/main/init.lua
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- Run gofmt/gofmpt, import packages automatically on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+    group = vim.api.nvim_create_augroup('setGoFormatting', { clear = true }),
+    pattern = '*.go',
+    callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 2000)
+        for _, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+                else
+                    vim.lsp.buf.execute_command(r.command)
+                end
+            end
+        end
+        vim.lsp.buf.format()
+    end
+})
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        -- vim.keymap.set('n', 'gd', "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        -- vim.keymap.set('n', '<leader>v', "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)
+        -- vim.keymap.set('n', '<leader>s', "<cmd>belowright split | lua vim.lsp.buf.definition()<CR>", opts)
+        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        -- vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, opts)
+        vim.keymap.set('n', 'grn', vim.lsp.buf.rename, opts)
+        -- vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+    end,
+})
---
+++ roles/nvim/files/lua/foo/plugins.lua
@@ -0,0 +1,240 @@
+------------------------------
+-- configurações de plugins --
+------------------------------
+
+-- todo: separar em arquivos.
+
+---------------------
+-- nvim-treesitter --
+---------------------
+vim.filetype.add({
+  extension = {
+    gotmpl = 'gotmpl',
+  },
+  pattern = {
+    ["*%.tmpl"] = "helm",
+  },
+})
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the four listed parsers should always be installed)
+    ensure_installed = {
+        "c", "lua", "vim", "go", "python", "javascript", "html", "css"
+        --[[ , "help" ]]
+    },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true
+    }
+}
+local treesitter_parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+treesitter_parser_config.templ = {
+    install_info = {
+        url = "https://github.com/vrischmann/tree-sitter-templ.git",
+        files = { "src/parser.c", "src/scanner.c" },
+        branch = "master",
+    },
+}
+
+vim.treesitter.language.register("templ", "templ")
+
+treesitter_parser_config.d2 = {
+    install_info = {
+        url = 'https://git.pleshevski.ru/pleshevskiy/tree-sitter-d2',
+        revision = 'main',
+        files = { 'src/parser.c', 'src/scanner.cc' },
+    },
+    filetype = 'd2',
+};
+vim.treesitter.language.register('d2', 'd2')
+
+vim.filetype.add({
+    extension = {
+        templ = "templ",
+    },
+})
+
+
+
+
+
+----------
+-- lsp ---
+----------
+
+local lspconfig = require('lspconfig')
+-- lspconfig.ts_ls.setup{
+--     settings = {
+--         implicitProjectConfiguration = {
+--             checkJs = true
+--         },
+--     }
+-- }
+lspconfig.templ.setup {}
+lspconfig.html.setup { filetypes = { "html", "templ", "cshtml", "javascript" }, }
+lspconfig.htmx.setup { filetypes = { "html", "templ" }, }
+
+vim.keymap.set("n", "<leader>lr", function()
+    vim.cmd [[ LspRestart ]]
+    print("Lsp restarted!")
+end, { noremap = true })
+
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+
+
+require("mason").setup {
+    log_level = vim.log.levels.DEBUG
+}
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+lsp.nvim_workspace()
+lsp.setup()
+
+
+if vim.fn.has('nvim-0.11.0') == 1 then
+    -- Usar a versão do lsp gerenciado pelo opam
+    vim.lsp.config['ocamllsp'] = {
+        cmd = { 'ocamllsp' },
+        filetypes = { 'ocaml' },
+        root_markers = { 'dune-project', 'dune' },
+    }
+    vim.lsp.enable('ocamllsp')
+end
+
+
+
+
+
+
+----------
+-- misc --
+----------
+
+-- trouble. diz o que deu de errado com o lsp.
+require('trouble').setup {
+    multiline = true,
+    -- mode = "document_diagnostics",
+}
+-- meu irmão em Cristo, eu não compreendo porque mudar a API completamente.
+-- vim.keymap.set('n', '<leader>t', '<cmd>TroubleToggle<CR>', {})
+vim.keymap.set('n', '<leader>t', '<cmd>Trouble diagnostics toggle<CR>', {})
+
+
+-- comment.nvim. comenta o seu código.
+require('Comment.ft').set('templ', { '//%s', '/*%s*/' })
+
+-- usei emacs por um tempinho e lá eles usavam essa keybind pra comentar
+vim.keymap.set('v', '<M-;>', 'gc', { remap = true })
+vim.keymap.set('n', '<M-;>', 'gcc', { remap = true })
+
+-- explorador de arquivos
+-- local oil_actions = require('oil.actions')
+require('oil').setup({
+    columns = {
+        "icon",
+        "permissions",
+        "size",
+        "mtime",
+    },
+    default_file_explorer = true,
+    delete_to_trash = true,
+    keymaps = {
+        ['h'] = 'actions.parent',
+        ['l'] = 'actions.select',
+        ['<C-j>'] = 'actions.select',
+        ['g.'] = 'actions.toggle_hidden',
+        ['gs'] = 'actions.change_sort',
+        ["S"] = "actions.open_terminal",
+        ["ss"] = 'actions.open_cmdline',
+        ["?"] = 'actions.show_help',
+        -- ['yy'] = 'actions.yank_entry',
+        ['gx'] = 'actions.open_external',
+    },
+    view_options = {
+        show_hidden = true,
+    },
+})
+
+-- barra de status
+require('lualine').setup()
+
+-- -- wildmenu melhorado
+-- local wilder = require 'wilder'
+-- wilder.setup {
+--     modes = {':', '/', '?'}
+-- }
+
+-- wilder.set_option('renderer', wilder.popupmenu_renderer(
+--   wilder.popupmenu_palette_theme({
+--     -- 'single', 'double', 'rounded' or 'solid'
+--     -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+--     border = 'rounded',
+--     max_height = '75%',      -- max height of the palette
+--     min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+--     prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+--     -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+--     reverse = 0,
+--   })
+-- ))
+
+require 'smartcolumn'.setup({
+    disabled_filetypes = { "help", "text", "markdown", "NvimTree", "lazy",
+        "mason", "help", "checkhealth", "lspinfo", "noice", "Trouble",
+        "fish", "zsh", "html", "templ" },
+    colorcolumn = "115"
+})
+
+
+-- require('slimv').setup()
+-- vim.g.slimv_swank_cmd = "!ros -e '(ql:quickload :swank) (swank:create-server)' wait &"
+-- vim.g.slimv_lisp = 'ros run'
+-- vim.g.slimv_impl = 'sbcl'
+
+-- require('example').setup {
+--     msg = 'foobar',
+-- }
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'LSP References' })
+
+
+
+local cfg = {
+    floating_window_off_x = 5, -- adjust float windows x position.
+    hint_prefix = "📍 ",
+    -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
+    floating_window_off_y = function()
+        local linenr = vim.api.nvim_win_get_cursor(0)[1] -- buf line number
+        local pumheight = vim.o.pumheight
+        local winline = vim.fn.winline() -- line number in the window
+        local winheight = vim.fn.winheight(0)
+
+        -- window top
+        if winline - 1 < pumheight then
+            return pumheight
+        end
+
+        -- window bottom
+        if winheight - winline < pumheight then
+            return -pumheight
+        end
+        return 0
+    end,
+}
+require "lsp_signature".setup(cfg)
